0b2a1ee7b74f783fc27233cb933f5368751abf6e
Observer <80362048+68756d616e@users.noreply.github.com>
2022-10-20 23:58:36 +0100

    Add files via upload

---
+++ ResultSeperatedAndAddedTogether.py
@@ -0,0 +1,11 @@
+# seperate your result and add it together
+#Below will prompt two digits
+abc = input("Please type a two digit number: ")
+#below takes the 1st digit
+a = abc[0]
+#Below takes the 2nd digit
+b = abc[1]
+# below adds the first digit and second digit
+c = int(a) + int(b)
+#Below will add both numbers and present the answer
+print(c)
